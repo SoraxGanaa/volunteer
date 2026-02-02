95dfe8e236229c2dbe6fe8ad65e49b1277f6360d
Gantulga <23b1num2846@stud.num.edu.mn>
2026-02-02 16:43:28 +0800

    env fix

--- src/db/kysely.ts
+++ src/db/kysely.ts
@@ -3,7 +3,11 @@ import { Pool } from "pg";
 import type { DB } from "./types";
 import dotenv from "dotenv";
 dotenv.config();
+const url = process.env.DATABASE_URL;
 
+if (!url || typeof url !== "string") {
+  throw new Error("DATABASE_URL is missing");
+}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
